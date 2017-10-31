ef5f1fba113379eb9f9fc6d21bced866eb1da18d
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-10-30 21:36:51 -0500

    02. WEB

--- src/main/java/modelo/Producto.java
+++ src/main/java/modelo/Producto.java
@@ -1,28 +1,27 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  PROYECTO SEGUNDO CORTE
+ *   co-Author :::   Juan Albarracin
+ *   co-Author :::  Mario Bolaños
+ *   co-Author ::: Sergio Orozco
+ *   co-Author :::  Brian Sterling
+ *     Program ::: Bases de Datos
+ *  Credential ::: SIST0008-G01:SIV
  */
+
 package modelo;
 
-import java.util.*;
-
-/**
- *
- * @author Mario-Bx
- */
 public class Producto extends Medidas
 {
-
     private int idProducto;
     private int idFKCotizacionProducto;
     private int numeroFKCotizacion;
     private int idFKFactura;
 
-    public ProductoJc() {
-    }
+    public Producto()
+    {}
 
-    public ProductoJc(int idProducto, int idFKCotizacionProducto, int idFKCotizacion, int idFKCotizacionFactuta, int Ancho, int Alto, int Grosor, int Cantidad) {
+    public Producto(int idProducto, int idFKCotizacionProducto, int idFKCotizacion, int idFKCotizacionFactuta, int Ancho, int Alto, int Grosor, int Cantidad)
+    {
         super(Ancho, Alto, Grosor, Cantidad);
         this.idProducto = idProducto;
         this.idFKCotizacionProducto = idFKCotizacionProducto;
@@ -30,45 +29,53 @@ public class Producto extends Medidas
         this.idFKFactura = idFKCotizacionFactuta;
     }
 
-    public int getIdProducto() {
+    public int getIdProducto()
+    {
         return idProducto;
     }
 
-    public void setIdProducto(int idProducto) {
+    public void setIdProducto(int idProducto)
+    {
         this.idProducto = idProducto;
     }
 
-    public int getIdFKCotizacionProducto() {
+    public int getIdFKCotizacionProducto()
+    {
         return idFKCotizacionProducto;
     }
 
-    public void setIdFKCotizacionProducto(int idFKCotizacionProducto) {
+    public void setIdFKCotizacionProducto(int idFKCotizacionProducto)
+    {
         this.idFKCotizacionProducto = idFKCotizacionProducto;
     }
 
-    public int getIdFKCotizacion() {
+    public int getIdFKCotizacion()
+    {
         return numeroFKCotizacion;
     }
 
-    public void setIdFKCotizacion(int idFKCotizacion) {
+    public void setIdFKCotizacion(int idFKCotizacion)
+    {
         this.numeroFKCotizacion = idFKCotizacion;
     }
 
-    public int getIdFKCotizacionFactuta() {
+    public int getIdFKCotizacionFactuta()
+    {
         return idFKFactura;
     }
 
-    public void setIdFKCotizacionFactuta(int idFKCotizacionFactuta) {
+    public void setIdFKCotizacionFactuta(int idFKCotizacionFactuta)
+    {
         this.idFKFactura = idFKCotizacionFactuta;
     }
 
-    @Override
-    public String toString() {
+    public String toString()
+    {
         return "ProductoJc{" + "idProducto=" + idProducto + ", idFKCotizacionProducto=" + idFKCotizacionProducto + ", idFKCotizacion=" + numeroFKCotizacion + ", idFKCotizacionFactuta=" + idFKFactura + '}';
     }
 
-    public int getprecio() {
+    public int getprecio()
+    {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
